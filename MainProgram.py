72b36cbbfd04d04606439fd6a716a224bd6583a8
ikendoit <virginman101@gmail.com>
2017-08-16 02:51:23 -0700

    modified to argv mainprogram

--- MainProgram.py
+++ MainProgram.py
@@ -1,27 +1,31 @@
 from lxml import etree 
 import grabmp3
+import create_xml
 import processed
+import sys
 
-mode = int(input("1.add songs \n2.download songs"))
-if (mode == 2 ):
-	parser =etree.XMLParser(remove_blank_text=True)
-	tree = etree.parse("data.xml",parser);
+if (len(sys.argv) ==3 ): 
+    print(sys.argv[1]+" and "+sys.argv[2]);
+    create_xml.addSong(sys.argv[1],sys.argv[2]);
+elif (len(sys.argv) ==2 ) : 
+    parser =etree.XMLParser(remove_blank_text=True)
+    tree = etree.parse("data.xml",parser);
 
-	videos = tree.getiterator('video')
+    videos = tree.getiterator('video')
 
-	for video in videos:
-		name =''
-		link =''
-		artist = ''
-		for element in video.getiterator('name'):
-			name = element.text;
-		for element in video.getiterator('link'): 
-			link = element.text;
-		for element in video.getiterator('artist'):
-			artist = element.text;
-		grabmp3.grab(name,link)
-		processed.backup(artist, name)
-		processed.delete(name)
+    for video in videos:
+        name =''
+        link =''
+        artist = ''
+        for element in video.getiterator('name'):
+                name = element.text;
+        for element in video.getiterator('link'): 
+                link = element.text;
+        for element in video.getiterator('artist'):
+                artist = element.text;
+        grabmp3.grab(name,link)
+        processed.backup(artist, name)
+        processed.delete(name)
 else : 
-	import create_xml
-	
+    create_xml.runPrompt();
+            
--- create_xml.py
+++ create_xml.py
@@ -5,66 +5,71 @@ from grablink import call_link
 from xml.dom import minidom
 from xml.dom.minidom import parseString
           
-id =0; 
-print("1: add song info \n2: delete song \n3:nothing, get me out.")
-option = int(input("input the option ")); 
-while (option != 3):
-	if (option == 1 ) : 
-		#get info and setup
-		print("getting the link: ")
-		video = etree.Element('video'); 
-		artist = etree.Element('artist');
-		name = etree.Element('name'); 
-		id = etree.Element('id');
-		status = etree.Element('none');
-		link = etree.Element('link');
-		#set up tree
-		video.append(artist); 
-		video.append(name);
-		video.append(id);
-		video.append(status);
-		video.append(link)
-		# input 
-		songName = str(raw_input("input name: "));
-		artistName = str(raw_input("input artist name: "));
+def addSong(songName,artistName):
+    #get info and setup
+    print("getting the link: ")
+    video = etree.Element('video'); 
+    artist = etree.Element('artist');
+    name = etree.Element('name'); 
+    ids = etree.Element('id');
+    status = etree.Element('none');
+    link = etree.Element('link');
+    #set up tree
+    video.append(artist); 
+    video.append(name);
+    video.append(ids);
+    video.append(status);
+    video.append(link)
 
-		name.text = songName; 
-		artist.text = artistName; 
-		status.text = "False";
-		link.text = call_link(songName,artistName)
+    name.text = songName; 
+    artist.text = artistName; 
+    status.text = "False";
+    link.text = call_link(songName,artistName)
 
-		#set data variable
-		# data = etree.tostring(video, pretty_print = True);
+    #set data variable
+    # data = etree.tostring(video, pretty_print = True);
 
-		#file appen 
-		parser =etree.XMLParser(remove_blank_text=True)
-		tree = etree.parse("data.xml",parser);
-		root = tree.getroot()
-		root.append(video)
+    #file appen 
+    parser =etree.XMLParser(remove_blank_text=True)
+    tree = etree.parse("data.xml",parser);
+    root = tree.getroot()
+    root.append(video)
 
-		f = open('data.xml','w')
-		# f.write(data+'\n')
-		f.write(etree.tostring(root,pretty_print=True))
-		f.close();
+    f = open('data.xml','w')
+    # f.write(data+'\n')
+    f.write(etree.tostring(root,pretty_print=True))
+    f.close();
 
-		print("1: add song info \n2: delete song \n3:nothing, get me out.")
-		option = int(input("input the option ")); 
+def runPrompt():
+    print("1: add song info \n2: delete song \n3:nothing, get me out.")
+    option = int(input("input the option ")); 
+    while (option != 3):
 
-	if (option == 2) : 
-		print("removing stuffs in data.xml")
-		f=open("data.xml",'r')
-		text = f.read();
-		tree = etree.fromstring(text)
-		f.close()
+        if (option == 1 ) : 
 
-		songName = str(raw_input("input name: "));
-		songName.strip();
-		videos = tree.xpath("//*[child::*[contains(text(),'"+songName+"')]]")
-		print(videos)
-		for video in videos : 
-			video.getparent().remove(video)
+            # input 
+            songName = str(raw_input("input name: "));
+            artistName = str(raw_input("input artist name: "));
 
-		f = open("data.xml",'w')
-		f.write(etree.tostring(tree,pretty_print = True, xml_declaration = True))
-		f.close()
-		option = int(input("input option"))
+            addSong(songName,artistName);
+            print("1: add song info \n2: delete song \n3:nothing, get me out.")
+            option = int(input("input the option ")); 
+
+        if (option == 2) : 
+            print("removing stuffs in data.xml")
+            f=open("data.xml",'r')
+            text = f.read();
+            tree = etree.fromstring(text)
+            f.close()
+
+            songName = str(raw_input("input name: "));
+            songName.strip();
+            videos = tree.xpath("//*[child::*[contains(text(),'"+songName+"')]]")
+            print(videos)
+            for video in videos : 
+                    video.getparent().remove(video)
+
+            f = open("data.xml",'w')
+            f.write(etree.tostring(tree,pretty_print = True, xml_declaration = True))
+            f.close()
+            option = int(input("input option"))
